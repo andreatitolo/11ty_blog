35d4fbf8f5eeea9f5baa9cff919bf96da6ee33ea
Andrea Titolo <titoloandrea@ik.me>
2023-06-22 19:42:04 +0200

    allow other loading options

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -11,7 +11,7 @@ module.exports = eleventyConfig => {
 
 	// Eleventy Image shortcode
 	// https://www.11ty.dev/docs/plugins/image/
-	eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, css, sizes) {
+	eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, css, sizes, loading) {
 		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
 		// Warning: Avif can be resource-intensive so take care!
 		let formats = ["webp", "jpeg"];
@@ -27,7 +27,7 @@ module.exports = eleventyConfig => {
 			class: css, 
 			alt,
 			sizes,
-			loading: "lazy",
+			loading: loading || "lazy",
 			decoding: "async",
 		};
 		return eleventyImage.generateHTML(metadata, imageAttributes);
